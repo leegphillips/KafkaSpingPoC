e25dbaba89b5f8218912daf0772394a3ad06acc4
leegphillips <leegphillips@yahoo.co.uk>
2017-10-06 10:22:28 +0100

    Added code from Spring Initializr and Travis connection

---
+++ src/main/java/com/github/leegphillips/KafkaSpringPoC/KafkaSpringPoCApplication.java
@@ -0,0 +1,12 @@
+package com.github.leegphillips.KafkaSpringPoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringPoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringPoCApplication.class, args);
+	}
+}
